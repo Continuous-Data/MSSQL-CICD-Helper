1e29d3c4fd24a39ac51323f0acaba1946d5c9a84
Tobi Steenbakkers <tobi.steenbakkers@gmail.com>
2018-04-06 14:00:01 +0200

    some renaming borrowed scripts from other module

--- Private/ImportConfig.ps1
+++ Private/ImportConfig.ps1
@@ -8,28 +8,28 @@ Function ImportConfig {
     ImportConfig
 #>
 
-if ( ( $null -ne $env:PSGitLabDomain) -and ( $null -ne $env:PSGitLabToken ) -and ( $null -ne $env:PSGitLabAPIVersion ) ) {
-    $Token = ConvertTo-SecureString -String $env:PSGitLabToken -AsPlainText -Force
-    [PSCustomObject]@{
-        Domain=$env:PSGitLabDomain
-        Token=$Token
-        APIVersion=$env:PSGitLabAPIVersion
+    # if ( ( $null -ne $env:PSGitLabDomain) -and ( $null -ne $env:PSGitLabToken ) -and ( $null -ne $env:PSGitLabAPIVersion ) ) {
+    #     $Token = ConvertTo-SecureString -String $env:PSGitLabToken -AsPlainText -Force
+    #     [PSCustomObject]@{
+    #         Domain=$env:PSGitLabDomain
+    #         Token=$Token
+    #         APIVersion=$env:PSGitLabAPIVersion
+    #     }
+    #     break;
+    # }
+
+    if ( $IsWindows -or ( [version]$PSVersionTable.PSVersion -lt [version]"5.99.0" ) ) {
+        $ConfigFile = "{0}\MSSQL-CICD-Helper\MSSQL-CICD-HelperConfiguration.xml" -f $env:appdata
+    } elseif ( $IsLinux ) {
+        $ConfigFile = "{0}/.MSSQL-CICD-Helper/MSSQL-CICD-HelperConfiguration.xml" -f $HOME
+    } else {
+        Write-Error "Unknown Platform"
     }
-    break;
-}
+    if (Test-Path $ConfigFile) {
+        Import-Clixml $ConfigFile
 
-if ( $IsWindows -or ( [version]$PSVersionTable.PSVersion -lt [version]"5.99.0" ) ) {
-    $ConfigFile = "{0}\PSGitLab\PSGitLabConfiguration.xml" -f $env:appdata
-} elseif ( $IsLinux ) {
-    $ConfigFile = "{0}/.psgitlab/PSGitLabConfiguration.xml" -f $HOME
-} else {
-    Write-Error "Unknown Platform"
-}
-if (Test-Path $ConfigFile) {
-    Import-Clixml $ConfigFile
-
-} else {
-    Write-Warning 'No saved configuration information. Run Save-GitLabAPIConfiguration.'
-    break;
-}
+    } else {
+        Write-Warning 'No saved configuration information found. Run Save-MSSQL-CICD-HelperConfiguration.'
+        break;
+    }
 }
