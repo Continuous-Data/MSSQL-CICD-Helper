cf1d01a0d37fcffa7f360f0d321c317de98c896e
Tobi Steenbakkers <tobi.steenbakkers@gmail.com>
2018-04-07 01:23:03 +0200

    fixed looping and testing for location

--- Public/Configuration/Save-MSSQL-CICD-HelperConfiguration.ps1
+++ Public/Configuration/Save-MSSQL-CICD-HelperConfiguration.ps1
@@ -1,45 +1,69 @@
 Function Save-GitLabAPIConfiguration {
 [cmdletbinding()]
     param(
-    # [Parameter(Mandatory=$true,
-    #            HelpMessage='You can find the token in your profile.',
-    #            Position=0)]
-    # [ValidateNotNullOrEmpty()]
-    # $Token,
+    [Parameter(Mandatory=$true,
+               HelpMessage='Path can be with or without including *.exe file. You can run Get-MSSQL-CICD-HelperPaths to find the paths for MSBUild / SQLPackage',
+               Position=0)]
+    [ValidateNotNullOrEmpty()]
+    $SQLPackageExePath,
 
-    # [Parameter(Mandatory=$true,
-    #            HelpMessage='Please provide a URI to the GitLab installation',
-    #            Position=1)]
-    # [ValidateNotNullOrEmpty()]
-    # [ValidatePattern("^(?:http|https):\/\/(?:[\w\.\-\+]+:{0,1}[\w\.\-\+]*@)?(?:[a-z0-9\-\.]+)(?::[0-9]+)?(?:\/|\/(?:[\w#!:\.\?\+=&%@!\-\/\(\)]+)|\?(?:[\w#!:\.\?\+=&%@!\-\/\(\)]+))?$")]
-    # $Domain,
+    [Parameter(Mandatory=$true,
+    HelpMessage='Path can be with or without including *.exe file. You can run Get-MSSQL-CICD-HelperPaths to find the paths for MSBUild / SQLPackage',
+    Position=1)]
+    [ValidateNotNullOrEmpty()]
+    $MSBuildExePath
 
-    # $APIVersion = 3
-        $test = 'test'
     )
 
-# if ( $Domain -match '^http:\/\/' ) {
-#     Write-Warning "All tokens will be sent over HTTP. Recommendation: Use HTTPS."
-# }
+    $PATHS = @{
+        MSBuild = $MSBuildExePath
+        SQLPackage = $SQLPackageExePath
+    }
+    
+    $pathstoappend = @()
+    Write-Output "modify"
+    $paths.Keys | ForEach-Object{
+        if(-not($paths[$_] -like '*.exe')){
+            $pathstoappend += $_
+        }
+    }
+    
+    $pathstoappend | ForEach-Object{
+        $paths[$_] = "$($paths[$_].trimend('\'))\$($_).exe"
+    }
+
+    # testing if either path exists
+    $paths.Keys | ForEach-Object {
+        if(-not(Test-Path (Split-Path $paths[$_]) ) ) {
+            Write-Error "Directory for either MSBuild or SQlPackage was not found."
+        }    
+    }
+    
+    
+
+    # Determine if *.exe was supplied or just a path.
 
     if ( $IsWindows -or ( [version]$PSVersionTable.PSVersion -lt [version]"5.99.0" ) ) {
         
+        # $Parameters = @{
+        #     Token=(ConvertTo-SecureString -string $Token -AsPlainText -Force)
+        #     Domain=$Domain;
+        #     APIVersion=$APIVersion;
+        # }
+        
         $Parameters = @{
-            Token=(ConvertTo-SecureString -string $Token -AsPlainText -Force)
-            Domain=$Domain;
-            APIVersion=$APIVersion;
+            MSBuildExe = $MSBuildExePath;
+            SQLPackageExe = $SQLPackageExePath
         }
         
         $ConfigFile = "$env:appdata\MSSQL-CICD-Helper\MSSQL-CICD-HelperConfiguration.xml"
 
     } elseif ( $IsLinux ) {
 
-        Write-Warning "Warning: Your GitLab token will be stored in plain-text on non-Windows platforms."
 
         $Parameters = @{
-            Token=$Token
-            Domain=$Domain;
-            APIVersion=$APIVersion;
+            MSBuildExe = $MSBuildExePath;
+            SQLPackageExe = $SQLPackageExePath
         }
         
         $ConfigFile = "{0}/.MSSQL-CICD-Helper/MSSQL-CICD-HelperConfiguration.xml" -f $HOME
