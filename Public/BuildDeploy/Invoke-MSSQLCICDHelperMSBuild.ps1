1a2f0d90e1d5e2f87fe100dd825560eb73d8cea4
Tobi Steenbakkers <tobi.steenbakkers@gmail.com>
2018-04-10 17:51:20 +0200

    First Working version of calling MSBuild w/o params

--- Public/BuildDeploy/Invoke-MSSQLCICDHelperMSBuild.ps1
+++ Public/BuildDeploy/Invoke-MSSQLCICDHelperMSBuild.ps1
@@ -1,12 +1,12 @@
 function Invoke-MSSQLCICDHelperMSBuild {
     [cmdletbinding()]
     param(
-        [Parameter(Mandatory=$true,
+        [Parameter(Mandatory=$false,
                HelpMessage='What to find: *.sln, *.dacpac, *.dtspac or *.sqlproject File. Options are: Solution, DacPac, DTSPack or Project',
                Position=0)]
         
         [ValidateScript({Test-Path -Path $_ -PathType Leaf})]
-        $Filepath,
+        $filename,
         [Parameter(Mandatory=$true,
                HelpMessage='Provides Build Arguments. Example /target:clean;build',
                Position=0)]
@@ -15,6 +15,29 @@ function Invoke-MSSQLCICDHelperMSBuild {
         $MSBuildArguments
     )
 
+    $configfile = ImportConfig 
 
+    if($null -eq $filename){
+        $curdir = Get-location
+        write-verbose "No filename given. Running Get-MSSQLCICDHelperFiletoBuildDeploy based to find the Solution in current script path $curdir"
+        $filename = Get-MSSQLCICDHelperFiletoBuildDeploy -typetofind 'Solution' -RootPath $curdir
+    }
+    else{
+        $filename = Get-ChildItem $filename
+    }
+    
+    Write-Verbose "The following file will be built: $($filename.Name) located in path $($filename.DirectoryName)"
+    
+    
+    Write-Verbose "The following build arguments will be used: $MSBuildArguments"
+    $configfile['MSBuildExe']
+
+    Write-Verbose "Constructing Command to build..."
+
+    $CommandtoExecute = "/k "" ""$($configfile['MSBuildExe'])"" ""$($filename.FullName)"" "
+
+    Write-Verbose "Command to be Executed is: cmd.exe $commandtoexecute"
+
+    Start-Process cmd.exe -ArgumentList $CommandtoExecute -NoNewWindow -PassThru
 }
 
