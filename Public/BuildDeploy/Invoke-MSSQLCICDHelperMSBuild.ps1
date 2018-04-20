7146859aa4f31552783960b81fd74339f2cdc84e
Tobi Steenbakkers <tobi.steenbakkers@gmail.com>
2018-04-20 15:43:16 +0200

    REWORK
    added try catch, new way of doing the params for invoke msbuild and restructured the output for this.

--- Public/BuildDeploy/Invoke-MSSQLCICDHelperMSBuild.ps1
+++ Public/BuildDeploy/Invoke-MSSQLCICDHelperMSBuild.ps1
@@ -22,6 +22,13 @@ function Invoke-MSSQLCICDHelperMSBuild {
         [switch] $hidden,
 
         [Parameter(Mandatory=$false,
+        HelpMessage='Switch to use the Invoke-MSBuild Module instead of built-in process.',
+        Position=0)]
+        #[Alias("Parameters","Params","P")]
+        [ValidateNotNullOrEmpty()]
+        [switch] $keeplogfiles,
+
+        [Parameter(Mandatory=$false,
                HelpMessage='Switch to use the Invoke-MSBuild Module instead of built-in process.',
                Position=0)]
         #[Alias("Parameters","Params","P")]
@@ -35,56 +42,85 @@ function Invoke-MSSQLCICDHelperMSBuild {
         [ValidateNotNullOrEmpty()]
         [String] $InvokeMSBuildParameters
     )
-
-    if($UseInvokeMSBuildModule){
-        if(-not(Get-Module Invoke-MSBuild)){
-            Write-Error 'Invoke-MSBuild was not found on this system. Make sure it is installed with Install-Module Invoke-MSBuild'
-            break;
-        }
-        
-    }
+    
     $result = @{}
-    $configfile = ImportConfig 
+    $result.CommandUsedToBuild = [string]::Empty
+    $result.MsBuildProcess = $null
+    $result.BuildSucceeded = $null
+    $result.Message = [string]::Empty
+	$result.BuildDuration = [TimeSpan]::Zero
+    $result.BuildLogFilePath = $null
+    $result.BuildLogFile = $null
 
-    if($null -eq $filename){
-        $curdir = Get-location
-        write-verbose "No filename given. Running Get-MSSQLCICDHelperFiletoBuildDeploy based to find the Solution in current script path $curdir"
-        $filename = Get-MSSQLCICDHelperFiletoBuildDeploy -typetofind 'Solution' -RootPath $curdir
-    }
-    else{
-        $filename = Get-ChildItem $filename
-    }
+    try{
+
     
-    Write-Verbose "The following file will be built: $($filename.Name) located in path $($filename.DirectoryName)"
-    
-    
-    Write-Verbose "The following build arguments will be used: $MSBuildArguments"
-    $configfile['MSBuildExe']
-
-    Write-Verbose "Constructing Command to build..."
-    if(-not($UseInvokeMSBuildModule)){
-
-        $CommandtoExecute = "/k "" ""$($configfile['MSBuildExe'])"" ""$($filename.FullName)"" $($MSBuildArguments) & Exit"" " 
-
-        Write-Verbose "Command to be Executed is: cmd.exe $commandtoexecute"
-        if($hidden){
-            $result = Start-Process cmd.exe -ArgumentList $CommandtoExecute -Wait -WindowStyle Hidden -PassThru
-        }else{
-            $result = Start-Process cmd.exe -ArgumentList $CommandtoExecute -Wait -NoNewWindow -PassThru
+        if($UseInvokeMSBuildModule){
+            if(-not(Get-Module Invoke-MSBuild)){
+                Write-Error 'Invoke-MSBuild was not found on this system. Make sure it is installed with Install-Module Invoke-MSBuild'
+                break;
+            }
+            
         }
-    }else{
-        if ($InvokeMSBuildParameters){
-            $command = "Invoke-MSBuild -Path $($filename.FullName) $($InvokeMSBuildParameters)"
-            $command
-            $result = Invoke-Expression $command
-        }else{
-            $result = Invoke-MSBuild -Path $($filename.FullName)
+
+        $configfile = ImportConfig 
+
+        if($null -eq $filename){
+            $curdir = Get-location
+            write-verbose "No filename given. Running Get-MSSQLCICDHelperFiletoBuildDeploy based to find the Solution in current script path $curdir"
+            $filename = Get-MSSQLCICDHelperFiletoBuildDeploy -typetofind 'Solution' -RootPath $curdir
+        }
+        else{
+            $filename = Get-ChildItem $filename
         }
         
+        Write-Verbose "The following file will be built: $($filename.Name) located in path $($filename.DirectoryName)"
+        
+        $logfile = "$($filename.FullName).msbuild.log"
+        $logbase = Split-Path -path $logfile -Parent
+        $result.BuildLogFilePath = $logbase
+        $result.BuildLogFile = $logfile 
+        
+        Write-Verbose "The following build arguments will be used: $MSBuildArguments"
+        $configfile['MSBuildExe']
+
+        Write-Verbose "Constructing Command to build..."
+        if(-not($UseInvokeMSBuildModule)){
+
+            $CommandtoExecute = "/k "" ""$($configfile['MSBuildExe'])"" ""$($filename.FullName)"" /fl /flp:logfile=""$($logfile)"" $($MSBuildArguments) & Exit"" " 
+
+            Write-Verbose "Command to be Executed is: cmd.exe $commandtoexecute"
+            $result.CommandUsedToBuild = "Command to be Executed is: cmd.exe $commandtoexecute"
+
+            if($hidden){
+                $result.MsBuildProcess = Start-Process cmd.exe -ArgumentList $CommandtoExecute -Wait -WindowStyle Hidden -PassThru
+            }else{
+                $result.MsBuildProcess = Start-Process cmd.exe -ArgumentList $CommandtoExecute -Wait -NoNewWindow -PassThru
+            }
+        }else{
+            if ($InvokeMSBuildParameters){
+                $CommandtoExecute = "Invoke-MSBuild -Path $($filename.FullName) -logdirectory $($logbase) $($InvokeMSBuildParameters)"
+            }else{
+                $CommandtoExecute = "Invoke-MSBuild -Path $($filename.FullName) -logdirectory $($logbase)"
+            }
+
+            if($keeplogfiles){
+                $CommandtoExecute += " -KeepBuildLogOnSuccessfulBuilds"
+            }
+            $result.CommandUsedToBuild = "Command to be Executed is: $commandtoexecute"
+            $result.MsBuildProcess = Invoke-Expression $CommandtoExecute
+        }
+    }catch{
+        $errorMessage = $_
+		$result.Message = "Unexpected error occurred while building ""$Path"": $errorMessage"
+        $result.BuildSucceeded = $false
+        Write-Error ($result.Message)
+        return $result
+        break;
     }
     
     Write-verbose "result exit code was: $($result.ExitCode)"
     #$result
-    $result.BuildSucceeded
+    $result
 }
 
