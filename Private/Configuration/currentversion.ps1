8e960a2445983a62ac9527ff509c3e144224be8f
Tobi Steenbakkers <tobi.steenbakkers@gmail.com>
2018-05-09 23:17:40 +0200

    added function for curent version

---
+++ Private/Configuration/currentversion.ps1
@@ -0,0 +1,13 @@
+Function CurrentVersion {
+    <#
+    .Synopsis
+       Returns version of the current software for checking config file etc.
+    .DESCRIPTION
+       Returns version of the current software for checking config file etc.
+    .EXAMPLE
+        ImportConfig
+    #>
+    
+    return 'v1.0.0'
+}
+    
