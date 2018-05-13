940ab0b86a0901007746171eebb06f8b8a872928
Tobi Steenbakkers <tobi.steenbakkers@gmail.com>
2018-05-13 21:29:09 +0200

    deleted output files

--- output/MSSQL-CICD-Helper/MSSQL-CICD-Helper.psm1
+++
@@ -1,3 +0,0 @@
-Write-Verbose 'Importing from [D:\users\steenbto\Documents\GitHub\MSSQL-CICD-Helper\MSSQL-CICD-Helper\private]'
-Write-Verbose 'Importing from [D:\users\steenbto\Documents\GitHub\MSSQL-CICD-Helper\MSSQL-CICD-Helper\public]'
-
