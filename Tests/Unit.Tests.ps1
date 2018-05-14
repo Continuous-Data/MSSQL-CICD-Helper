f0c8fdab69259498c9d6aa41fcb0a6e9531fca17
Tobi Steenbakkers <tobi.steenbakkers@gmail.com>
2018-05-14 19:58:37 +0200

    Added pester for not found config

--- Tests/Unit.Tests.ps1
+++ Tests/Unit.Tests.ps1
@@ -6,7 +6,43 @@ $moduleName = Split-Path $moduleRoot -Leaf
 
 Describe "Basic function unit tests" -Tags Build {
 
-    Context "importconfig" {
+    Context "Config" {
+
+        it "importconfig should return a value when a config file exists" {
+
+            { Get-MSSQLCICDHelperConfiguration } | Should Not Throw
+        }
+
+    }
+
+    Context "NoConfig" {
+        BeforeAll{
+            if(Test-path $env:appdata\MSSQLCICDHelper\MSSQLCICDHelperConfiguration.xml){
+                Rename-Item -Path "$env:appdata\MSSQLCICDHelper\MSSQLCICDHelperConfiguration.xml" -NewName "PesterBackupMSSQLCICDHelperConfiguration.xml"
+            }
+        }
+
+        AfterAll{
+            if(Test-path $env:appdata\MSSQLCICDHelper\PesterBackupMSSQLCICDHelperConfiguration.xml){
+                Rename-Item -Path "$env:appdata\MSSQLCICDHelper\PesterBackupMSSQLCICDHelperConfiguration.xml" -NewName "MSSQLCICDHelperConfiguration.xml"
+            }
+        }
+
+        It "When no config file is found the following functions should fail"{
+            { Get-MSSQLCICDHelperConfiguration } | Should Throw
+        }
+    }
+
+    Context "Build" {
+
+        it "importconfig should return a value when a config file exists" {
+
+            { Get-MSSQLCICDHelperConfiguration } | Should Not Throw
+        }
+
+    }
+
+    Context "Deploy" {
 
         it "importconfig should return a value when a config file exists" {
 
