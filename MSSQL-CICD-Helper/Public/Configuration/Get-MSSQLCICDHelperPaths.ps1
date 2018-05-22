da0d4cfc5b6316c00ae4d88b9eedacaa8b60f86a
Tobi Steenbakkers <tobi.steenbakkers@gmail.com>
2018-05-22 11:46:19 +0200

    see if this fixes the issue on APPveyor

--- MSSQL-CICD-Helper/Public/Configuration/Get-MSSQLCICDHelperPaths.ps1
+++ MSSQL-CICD-Helper/Public/Configuration/Get-MSSQLCICDHelperPaths.ps1
@@ -80,6 +80,7 @@ Function Get-MSSQLCICDHelperPaths {
         }
     }
     Write-verbose "searching for $exestofind in $rootpath"
+    $results = @{}
 
     $exestofind | ForEach-Object{
         $results += Get-ChildItem -Path $rootpath -filter $_ -Recurse -ErrorAction SilentlyContinue
