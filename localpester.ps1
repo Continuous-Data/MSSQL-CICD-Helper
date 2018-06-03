aeebdaeea0d889daffc6bfe750a0fe2d134f950e
Tobi Steenbakkers <tobi.steenbakkers@gmail.com>
2018-06-03 10:28:25 +0200

    see if this works for PSSA

--- localpester.ps1
+++ localpester.ps1
@@ -7,7 +7,8 @@ Derived from scripts written by Warren F. (RamblingCookieMonster)
 [cmdletbinding()]
 param ($Task = 'LocalPester')
 "Starting build"
-
+" Installing latest NuGet Provider"
+Install-PackageProvider Nuget -Force
 # Grab nuget bits, install modules, set build variables, start build.
 "  Install Dependent Modules"
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
