629b8d8cd5a838f233a5c53697d54b1d54ad0e85
Tobi Steenbakkers <tobi.steenbakkers@gmail.com>
2018-06-03 20:37:32 +0200

    xD

--- localpester.ps1
+++ localpester.ps1
@@ -8,12 +8,12 @@ Derived from scripts written by Warren F. (RamblingCookieMonster)
 param ($Task = 'LocalPester')
 "Starting build"
 # " Installing latest NuGet Provider"
-Install-PackageProvider -name NuGet -Force
+#Install-PackageProvider -name NuGet -Force
 # Grab nuget bits, install modules, set build variables, start build.
 "  Install Dependent Modules"
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 Install-Module InvokeBuild, BuildHelpers, PSDeploy, PSScriptAnalyzer, PlatyPS -force -Scope CurrentUser
-Install-Module Pester -Force -SkipPublisherCheck -Scope CurrentUser
+Install-Module PSScriptAnalyzer, Pester -Force -SkipPublisherCheck -Scope CurrentUser
 
 "  Import Dependent Modules"
 $modules = New-Object System.Collections.ArrayList
